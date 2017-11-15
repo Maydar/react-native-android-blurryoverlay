2119fdf041b4884972689677110dd136fad18307
Maydar <biohazard1994@mail.ru>
2017-11-15 13:04:59 +0300

    Update BlurryOverlayPackage.java

--- src/main/java/com/kwaak/react/BlurryOverlayPackage.java
+++ src/main/java/com/kwaak/react/BlurryOverlayPackage.java
@@ -22,11 +22,6 @@ public class BlurryOverlayPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         Log.i("BlurryOverlayView", "Created view managers");
         return Arrays.<ViewManager>asList(
